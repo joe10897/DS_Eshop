a43d88626a54742a839e6cb2482304859ae85ce2
NG-KWAN-LOK <t106590052@ntut.org.tw>
2020-09-17 13:22:32 +0800

    no message

--- web/css.php
+++
@@ -1,3 +0,0 @@
-  <link rel="stylesheet" href="css/style.css" ><!-- style.css -->
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@100;300;400;500;700;900&display=swap" rel="stylesheet"/>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
